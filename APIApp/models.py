8c1afe3f9d4fc92dce145e63daaf00c052f7d4cb
PanisaJ <s5701012630140@email.kmutnb.ac.th>
2019-04-26 06:17:08 +0700

    Added User model and foreignKey

--- APIApp/models.py
+++ APIApp/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=40)
+    def __str__(self):
+        return self.username
+
 class Image(models.Model):
-    username = models.CharField(max_length=200,default='')
+    user = models.ForeignKey(User, on_delete=models.CASCADE,default='')
     photo = models.ImageField(blank=False,null=False)
     caption = models.TextField(null=True)
     def __str__(self):
         return self.photo.name
 
 class Comment(models.Model):
-    photoID = models.IntegerField(default=0)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE,default='')
     comment_text = models.CharField(max_length=200,default='')
     def __str__(self):
         return self.comment_text
+
+
+
